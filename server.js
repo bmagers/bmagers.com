181ac136d7a02ae7519ffdc513de3ee7e1a09f77
Bryan Magers <bmagers@users.noreply.github.com>
2019-01-02 19:28:44 -0800

    resume route

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) =>
   res.send("index.html")
 );
 
+app.get("/resume", (req, res) =>
+  res.send("assets/pdf/resume.pdf")
+);
+
 app.listen(port, () =>
   console.log(`Server listening on port ${port}`)
 );
